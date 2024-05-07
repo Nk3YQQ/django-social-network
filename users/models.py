e8119984c941a1e364a86294601867e49d03c33c
egor <ega.nik5674@gmail.com>
2024-05-07 18:38:05 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class Gender(models.TextChoices):
+    MALE = 'male', 'Мужской'
+    FEMALE = 'female', 'Женский'
 
 
 class User(AbstractUser):
@@ -14,5 +20,7 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    gender = models.CharField(max_length=50, choices=Gender.choices, verbose_name='Пол')
+    birthday_date = models.DateTimeField(verbose_name='Дата рождения')
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
     phone = models.IntegerField(verbose_name='Номер телефона', **NULLABLE)
